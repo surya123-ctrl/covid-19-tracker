afc4de677197c0054b7037897fa5faf8992f24fe
Surya <suryatomar303@gmail.com>
2022-05-13 12:16:53 +0530

    Select countries bar done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
+import { FormControl, MenuItem, Select } from '@mui/material';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  }
   return (
     <div className="App">
-      <h1>Covid 19 Tracker</h1>
+      <div className='app-header'>
+        <h1>Covid-19 Tracker</h1>
+        <FormControl className='app-dropdown'>
+          <Select variant='outlined' onChange={onCountryChange} value={country} >
+            <MenuItem value='worldwide'>Worldwide</MenuItem>
+            {
+              countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+      </div>
+
+      <div className='app-stats'>
+        {/* infobox */}
+        {/* infobox */}
+        {/* infobox */}
+
+      </div>
+      {/* table */}
+      {/* graph */}
+      {/* map */}
     </div>
   );
 }
